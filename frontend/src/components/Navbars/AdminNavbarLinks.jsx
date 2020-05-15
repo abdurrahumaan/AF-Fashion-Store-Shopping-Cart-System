d2491ca1c9d3c5ed98a59c87633ed327b16c7baf
abdurrahumaan <abdurrahumaannazeer12345@gmail.com>
2020-05-15 14:20:22 +0530

    add components

---
+++ frontend/src/components/Navbars/AdminNavbarLinks.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { NavItem, Nav, NavDropdown, MenuItem } from "react-bootstrap";
+
+class AdminNavbarLinks extends Component {
+  render() {
+
+    return (
+      <div>
+        <Nav>
+          <NavItem eventKey={3} href="#">
+            <i className="fa fa-search" />
+            <p className="hidden-lg hidden-md">Search</p>
+          </NavItem>
+        </Nav>
+        <Nav pullRight>
+          <NavItem eventKey={1} href="#">
+            Account
+          </NavItem>
+          <NavItem eventKey={3} href="#">
+            Log out
+          </NavItem>
+        </Nav>
+      </div>
+    );
+  }
+}
+
+export default AdminNavbarLinks;
