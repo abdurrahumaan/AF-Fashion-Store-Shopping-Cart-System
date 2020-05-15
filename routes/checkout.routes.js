a7d58b7a801dd42b35f0220869d866540c68045a
Koshana <53309623+Koshana@users.noreply.github.com>
2020-05-15 12:33:25 +0530

    Delete checkout.routes.js

--- routes/checkout.routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-    const checkoutController = require("../controller/checkout.controller");
-    var router = require("express").Router();
-
-    router.post("/checkout", checkoutController.checkout);
-
-    app.use('/api/checkout', router);
-};
