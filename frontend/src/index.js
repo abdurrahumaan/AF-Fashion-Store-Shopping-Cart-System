ac2c873a7736d6ebe5f1781b09b6df971b94d65f
abdurrahumaan <abdurrahumaannazeer12345@gmail.com>
2020-05-15 14:03:08 +0530

    add routes

---
+++ frontend/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/animate.min.css";
+import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
+import "./assets/css/demo.css";
+import "./assets/css/pe-icon-7-stroke.css";
+
+// import StoreLayout from "layouts/Admin";
+// import StoreLayout from "layouts/Admin";
+import LoginLayout from "layouts/Home"
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" render={props => <LoginLayout {...props} />} />
+      <Redirect from="/" to="/home" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
---
+++ frontend/src/processRoutes.js
@@ -0,0 +1,13 @@
+import UserLogin from "layouts/Auth"
+
+const processRoutes = [{
+        path: "/userLogin",
+        name: "User Login",
+        icon: "pe-7s-graph",
+        component: UserLogin,
+        layout: "/user"
+    },
+
+];
+
+export default processRoutes
