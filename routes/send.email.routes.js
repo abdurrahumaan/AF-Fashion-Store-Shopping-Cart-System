69b70be9154d8c01d2965dc0d2a9ab195563ff1b
Koshana <53309623+Koshana@users.noreply.github.com>
2020-05-15 12:33:53 +0530

    Delete send.email.routes.js

--- routes/send.email.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-    const sendEmailController = require("../controller/send.email.controller");
-    var router = require("express").Router();
-  
-    router.post("/send", sendEmailController.process);
-  
-    app.use('/api/sendemail', router);
-  };
-  
